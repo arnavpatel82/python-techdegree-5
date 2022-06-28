7d765df3978d5c88686fa8c8db837f0a4d718b24
arnavpatel82 <arnav.patel82@yahoo.com>
2022-06-28 22:42:34 +0800

    created routes in app.py and created layout.html

--- app.py
+++ app.py
@@ -4,7 +4,23 @@ from models import db, Project, app
 
 @app.route('/')
 def index():
+    # projects = Project.query.all()
     return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/detail')
+def project():
+    return render_template('detail.html')
+
+
+@app.route('/add-project')
+def add_project():
+    return render_template('projectform.html')
 
 # @app.route('/projects/new', methods=['GET', 'POST'])
 # def add_project():
